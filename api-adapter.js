1871bc998e77c289cd82b4ad7f3cc793ecb0c336
yungpanko <jrjohns@mit.edu>
2017-07-11 17:31:59 -0400

    send post requests

--- api-adapter.js
+++ api-adapter.js
@@ -25,3 +25,20 @@ function getSingleSubmission(id, callback) {
     .catch(error => console.log(error))
 
 }
+
+function postNewSubmission(data) {
+
+  const url = 'http://localhost:3000/submissions'
+
+  fetch(url, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
+
+}
--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-$(document).ready(function () {
-  // call functions here
-  getAllSubmissions(displayContent)
-});
+$(document)
+  .ready(function () {
+    // call functions here
+    getAllSubmissions(displayContent)
+    submitForm()
+  });
 
 function displayContent(data) {
   let collection = new Collection(data)
   collection.render()
+}
+
+function submitForm() {
+  $('#form')
+    .on('submit', function (event) {
+      let title = $('#form > input[name="title"]').val()
+      let description = $('#form > input[name="description"]').val()
+      let body = $('#form > input[name="body"]').val()
+      let data = {
+        "submission": {
+          "title": title,
+          "description": description,
+          "body": body
+        }
+      }
+      postNewSubmission(data)
+      collection.render()
+      event.preventDefault()
+    })
+
 }
