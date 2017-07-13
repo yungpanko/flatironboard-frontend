90067a4f5057bc357543c090c996aa142be48559
sarahsweat <sarahsweat@gmail.com>
2017-07-13 17:41:51 -0400

    started like functionality

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ $(document)
     getAllContentTypes(renderContentTypeDropDown) //move out of document ready when we move the form
     submitForm()
     loadMore()
+    heartListener()
 
   })
 
@@ -40,6 +41,7 @@ function renderContentTypeDropDown(data) {
 }
 
 function displayContent(data) {
+  $('#results > div').html('')
   let collection = new Collection(data)
   let array = collection.submissionsPage(page)
   collection.render(array)
@@ -66,5 +68,18 @@ function submitForm() {
       postNewSubmission(data)
       collection.render()
       event.preventDefault()
+    })
+}
+
+function heartListener () {
+    $('#results').on('click', '#heart', function (event) {
+      event.preventDefault()
+      let data = {
+        "submission": {id: $(this).children()[0].value}
+      }
+      incrementLikes($(this).children()[0].value, data)
+      console.log("I was clicked")
+      page = 1
+      getAllSubmissions(displayContent)
     })
 }
--- submission.js
+++ submission.js
@@ -6,8 +6,10 @@ class Submission {
     this.body = object.body
     this.src_url = object.src_url
     this.link_url = object.link_url
+    this.likes = object.likes
     this.category = object.category.name
     this.content_type = object.content_type.name
+
   }
 
   render() {
@@ -26,13 +28,24 @@ class Submission {
             ${this.body.substring(0,150)} ...
           </div>
           <a href="${this.link_url}" target="_blank" >
-          <div class="ui bottom attached button">
-            <p>View Source</p>
-          </div>
+            <div class="ui bottom attached button">
+              <p>View Source</p>
+            </div>
           </a>
         </div>
+        <div class="center aligned extra content">
+          <div class="ui labeled button" tabindex="0">
+            <div class="ui red button" id="heart">
+              <input type="hidden" value="${this.id}">
+              <i class="heart icon"></i> Like
+            </div>
+            <a class="ui basic red left pointing label">
+              ${this.likes}
+            </a>
+          </div>
+        </div>
       </div>
-      </div>
+    </div>
     `
 
   }
