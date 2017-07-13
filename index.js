c608476ce8ce155911ced20ed4c5664224b4f921
yungpanko <jrjohns@mit.edu>
2017-07-13 14:12:08 -0400

    added search

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let page = 1
+let term
 
 $(document)
   .ready(function () {
@@ -9,19 +10,38 @@ $(document)
     getAllContentTypes(renderContentTypeDropDown) //move out of document ready when we move the form
     submitForm()
     loadMore()
-
+    search()
   })
 
-  function loadMoreSubmissions(data){
-    let collection = new Collection(data)
-    let array = collection.submissionsPage(page)
-    collection.render(array)
-    page += 1
 
-  }
+function search() {
+  $('#search').on('search', function (event) {
+    term = $(this).val()
+    getAllSubmissions(searchFilter)
+    //send term to API and get back results
+  })
+}
 
-function loadMore () {
-  $('#loadMoreButton').on('click', function(event) {
+function searchFilter(data) {
+  page = 1
+  let collection = new Collection(data)
+  collection.searchResults(term)
+  let array = collection.submissionsPage(page)
+  $('#results > div').html('')
+  collection.render(array)
+  page += 1
+}
+
+function loadMoreSubmissions(data) {
+  let collection = new Collection(data)
+  let array = collection.submissionsPage(page)
+  collection.render(array)
+  page += 1
+
+}
+
+function loadMore() {
+  $('#loadMoreButton').on('click', function (event) {
     getAllSubmissions(loadMoreSubmissions)
   })
 
